3ace242837883e72f12b19ffa03d0d9a5affa011
Anderson Paulo <andersonpauloti@gmail.com>
2023-03-19 12:22:07 -0300

    Adicionando segundo botao e power

--- scripts/playerAnin.js
+++ scripts/playerAnin.js
@@ -1,25 +1,88 @@
 const MaxFrame = document.querySelectorAll(".player ul li").length-1
 const Player = document.querySelector("#home-page .player ul")
-const Frame = document.querySelectorAll("ul img")
+const Frame = document.querySelectorAll(".player ul li img")
+const FramePower = document.getElementsByClassName("power")
+const MaxFramePower = document.getElementsByClassName("power").length-1
 const Fundo = document.querySelector("#home-page .content .fundo")
 const FundoWidth = document.querySelector(".content .fundoMov1")
 const ButtonPlay = document.querySelector(".box2 .button")
+const ButtonPower = document.querySelector(".box4 .button2")
 let interval = 0
+let interval2 = 0
 let cont = 0
+let time = 0
+let fly = true
 
 function AninPlayer(){
 
-    let AninInterval = setInterval(function(){
-        
-        Frame[interval].style.display= 'none'
-        interval++
-        if(interval > MaxFrame){
-            interval = 0
-        }
-        Frame[interval].style.display = 'block'
+    let AninInterval = setInterval(function(){ 
+            time++
+            Frame[interval].style.display= 'none'     
+            FramePower[interval2].style.display= 'none'
+            interval++
+          
+            if(interval >5){
+                interval = 0
+            }
+            if(fly==false){
+                interval2++
+                if(interval2 > MaxFramePower){
+                    interval2 = 0
+                } 
+               
+            }
+            if(fly==true){
+                
+                Frame[interval].style.display= 'flex' 
+            
+            }
+            
+            document.addEventListener("keydown",(Event)=>{
+                if(Event.shiftKey===true){  
+                     Frame[interval].style.display= 'none'                                                    
+                     fly = false                   
+                    
+                    
+                    if(interval2 > MaxFramePower){
+                        interval2 = 0
+                    }         
+                FramePower[interval2].style.display= 'flex'   
+            }           
+            })
+            
+            document.addEventListener("keyup",(Event)=>{
+                if(Event.shiftKey === false){
+                    
+                    fly = true                    
+                    FramePower[interval2].style.display= 'none'
+                }
+            }) 
+            
+            ButtonPower.addEventListener("touchstart",()=>{
+                    ButtonPower.style.backgroundColor ="green"
+                    Frame[interval].style.display= 'none'                                                    
+                    interval2++
+                
+                    if(interval2 > MaxFramePower){
+                        interval2 = 0
+                    }         
+                    FramePower[interval2].style.display= 'flex'
+                        
 
-    },100)   
+            },)
+
+            ButtonPower.addEventListener("touchend",()=>{
+                    
+                    ButtonPower.style.backgroundColor ="yellow"
+                    //FramePower[interval2].style.display= 'none'
+                    
+            })
+                  
+
+    },100)       
 }
+
+
 
 function MotionPlayer(){
    
